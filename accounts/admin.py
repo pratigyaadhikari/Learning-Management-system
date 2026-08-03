9dc1fcb049187490b052b9584a5e78a465270fb9
Pratigya Adhikari <pratigya144@gmail.com>
2026-08-03 17:09:19 +0545

    custom user models and profiles models

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import *
+
+
+# User admin 
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id","username", "role",)
 
 # Register your models here.
+@admin.register(StudentProfile)
+class StudentProfileAdmin(admin.ModelAdmin):
+    list_display = ("id","user","interests",)
+    
+
+@admin.register(InstructorProfile)
+class InstructorProfileAdmin(admin.ModelAdmin):
+    list_display = ("id","user","title",)
+    
+   
+@admin.register(SponsorProfile)
+class SponsorProfileAdmin(admin.ModelAdmin):
+    list_display = ("id","user","company_name",)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    class Roles(models.TextChoices):
+        ADMIN = 'ADMIN', 'Admin'
+        INSTRUCTOR = 'INSTRUCTOR', 'Instructor'
+        STUDENT = 'STUDENT', 'Student'
+        SPONSOR = 'SPONSOR', 'Sponsor'
+    role = models.CharField(max_length=20, choices=Roles.choices, default=Roles.STUDENT)
+    
+    def __str__(self):
+        return self.username
+    
+    
+class StudentProfile(models.Model):
+    user =models.OneToOneField(User, on_delete=models.CASCADE, related_name='student_profile')
+    interests = models.TextField(blank=True)
+    
+    def __str__(self):
+        return f"{self.user.username}'s Student Profile"
+    
+class InstructorProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='instructor_profile')
+    bio = models.TextField(blank=True)
+    title = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f"Instructor Profile: {self.user.username}"
+    
+class SponsorProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="sponsor_profile")
+    company_name = models.CharField(max_length=100)
+    company_website = models.URLField(blank=True)
+    
+    def __str__(self):
+        return f"Sponsor Profile: {self.company_name}"    
