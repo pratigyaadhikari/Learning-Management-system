6d912f5c4c6e39e8aee5f6c34d7414f5baab724c
Pratigya Adhikari <pratigya144@gmail.com>
2026-08-03 23:27:09 +0545

    add course model and admin

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from accounts.models import InstructorProfile
 # Create your models here.
+class Course(models.Model):
+    class Difficulty(models.TextChoices):
+        BEGINNER = "BEGINNER","Beginner"
+        INTERMEDIATE = "INTERMEDIATE","Intermediate"
+        ADVANCED = "ADVANCED","Advanced"
+        
+    difficulty = models.CharField(max_length=50, choices=Difficulty.choices, default=Difficulty.BEGINNER)    
+    
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    instructor = models.ForeignKey(InstructorProfile, on_delete=models.CASCADE, related_name="courses_taught") 
+    price = models.DecimalField(max_digits=8,decimal_places=2, default=0.00) 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
